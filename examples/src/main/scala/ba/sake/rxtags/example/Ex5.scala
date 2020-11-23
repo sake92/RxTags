9fa7e302b20ed143946d9e6b367a03ab32f4d9dd
Sakib Hadžiavdić <sakib@sake.ba>
2020-11-23 14:09:09 +0100

    Add Ex5

---
+++ examples/src/main/scala/ba/sake/rxtags/example/Ex5.scala
@@ -0,0 +1,26 @@
+package ba.sake.rxtags.example
+
+import ba.sake.rxtags._
+import org.scalajs.dom
+import scalatags.JsDom.all._
+
+object Ex5 {
+
+  val ticker$ = Var(0)
+
+  dom.window.setInterval(
+    () => {
+      println("tick")
+      ticker$.set(ticker$.now + 1) // or counter$.set(_ + 1)
+    },
+    1000
+  )
+
+  def content(): Frag =
+    div(
+      h1("Example 5"),
+      ticker$.map { c =>
+        s"Ticker: $c"
+      }.asFrag
+    )
+}
