95655d85a01e651dced7b1e0083cfcda58dc1412
Sakib Hadžiavdić <sakib@sake.ba>
2020-11-27 22:20:07 +0100

    Prettify examples

---
+++ examples/src/main/scala/ba/sake/rxtags/example/Ex6.scala
@@ -0,0 +1,41 @@
+package ba.sake.rxtags.example
+
+import ba.sake.rxtags._
+import org.scalajs.dom
+import scalatags.JsDom.all._
+
+// RxTags variable list
+object Ex6 extends Example {
+
+  case class CartItem(count: Int, name: String)
+
+  val cartItems$ = Var(
+    List(
+      CartItem(10, "eggs"),
+      CartItem(1, "milk"),
+      CartItem(3, "bananas"),
+      CartItem(2, "icecreams")
+    )
+  )
+
+  def content = div(
+    cartItems$.map { items =>
+      ul(
+        items.map { item =>
+          li(
+            b(item.count),
+            s" ${item.name} ",
+            button(onclick := delete(item))("Delete")
+          )
+        }
+      )
+    }.asFrag
+  )
+
+  def delete(item: CartItem): (dom.Event => Unit) =
+    e => {
+      cartItems$.set { currentItems =>
+        currentItems.filterNot(_ == item)
+      }
+    }
+}
